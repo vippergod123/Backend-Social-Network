c288d4cd16f6302fc9c5f235b445a7056eebd989
Cris <duytruong0702@gmail.com>
2018-12-10 16:30:45 +0700

    Duy fix bug

--- routes/create_account.js
+++ routes/create_account.js
@@ -10,25 +10,25 @@ router.post('/', function(req, res, next) {
     console.log(req.query.public_key);
     console.log(blockchainKey.public_key);
 
-  var encodeCreateAccount =   handleTransaction.encodeCreateAccountTransaction(blockchainKey.public_key,req.query.public_key,blockchainKey.private_key);
-  var encodePayment =  handleTransaction.encodePaymentTransaction(blockchainKey.public_key,public_key,100);
+  var encodeCreateAccount =  handleTransaction.encodeCreateAccountTransaction(blockchainKey.public_key,req.query.public_key,blockchainKey.private_key);
+  var encodePayment =  handleTransaction.encodePaymentTransaction(blockchainKey.public_key,req.query.public_key,100,blockchainKey.private_key);
 
   console.log(encodeCreateAccount);
   console.log(encodePayment);
   
-  var broadcastRequest = "https://komodo.forest.network/broadcast_tx_commit?tx="
+//   var broadcastRequest = "https://komodo.forest.network/broadcast_tx_commit?tx="
   
 
-    axios.get(broadcastRequest+encodeCreateAccount)
-  .then(response => {
-      axios.get(broadcastRequest+encodePayment)
-      console.log(response.data);
-      res.json(response.data)
-  })
-  .then(response => { console.log(response.data); res.json(response.data)})
-  .catch(error => {
-    console.log(error);
-  });
+//     axios.get(broadcastRequest+encodeCreateAccount)
+//   .then(response => {
+//       axios.get(broadcastRequest+encodePayment)
+//       console.log(response.data);
+//       res.json(response.data)
+//   })
+//   .then(response => { console.log(response.data); res.json(response.data)})
+//   .catch(error => {
+//     console.log(error);
+//   });
    
 });
 
