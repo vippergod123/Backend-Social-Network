a6f64df101d6d5842ada35e2c7acad3b5ca834e9
Cris <duytruong0702@gmail.com>
2018-12-10 15:46:04 +0700

    Duy - Create Payment

---
+++ routes/payment.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+const blockchainKey = require('../config/blockchainKey');
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+
+    console.log(req.query.public_key);
+    
+ 
+  axios.get(getTransactionFromPublicNode)
+  .then(resp => {
+      console.log(resp);
+      
+    res.json(
+      resp.data
+    )
+  })
+  .catch(error => {
+    console.log(error);
+  });
+   
+});
+
+module.exports = router;
