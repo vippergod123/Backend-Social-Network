fc7a9528e4f8c2c97987e528be2e1f7d286dc807
Cris <duytruong0702@gmail.com>
2018-12-26 14:49:55 +0700

    DUy test

--- routes/broadcast_commit.js
+++ routes/broadcast_commit.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const Domain = require('../config/nodePublic');
-
+const {publicDomain} = require('../Global/Variable/PublicNodeDomain');
 // Middleware
 const {isLoggedin} = require('../Global/Function/middleware');
 
 router.post('/', isLoggedin, function(req, res, next) {
-  var broadcastRequest = Domain.komodoDomain + "broadcast_tx_commit?tx=" + req.body.enCodeTransaction;
+  
+  
+  var broadcastRequest = publicDomain + "/broadcast_tx_commit?tx=" + req.body.enCodeTransaction;
+  console.log(broadcastRequest);
+  
   axios.get(broadcastRequest)
     .then((response) => {
       if (response.data.result.height != '0') {
