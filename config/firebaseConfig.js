593f7823d7d24922b2560f80e39a6cda1d1c141e
Vippergod123 <trantandat130497@gmail.com>
2018-12-27 09:16:57 +0700

    server test

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,15 +1,12 @@
 const admin = require('firebase-admin');
 // const serviceAccount = require('./ServiceAccountKey.json');
-// const serviceAccount = require('./SecondService.json');
-const serviceAccount = require('./ThirdService.json');
+const serviceAccount = require('./SecondService.json');
+// const serviceAccount = require('./ThirdService.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 })
 
 admin.firestore().settings({timestampsInSnapshots:true});
-
-
 const firestore = admin.firestore();
-
 module.exports.firestore = firestore
