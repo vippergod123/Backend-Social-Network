241fc2a99d1900f1fd5293121b9401ab8acfd08c
Vippergod123 <trantandat130497@gmail.com>
2018-12-27 09:48:45 +0700

    fix bug

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 // const serviceAccount = require('./ServiceAccountKey.json');
-const serviceAccount = require('./SecondService.json');
-// const serviceAccount = require('./ThirdService.json');
+// const serviceAccount = require('./SecondService.json');
+const serviceAccount = require('./ThirdService.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
